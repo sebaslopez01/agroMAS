25e512e2883f46a586fef9a69f6c5a5b8fa655fa
abimont <abimontes00@gmail.com>
2023-02-06 19:17:33 -0500

    Add components and feature

--- pages/investments.tsx
+++ pages/investments.tsx
@@ -1,13 +1,15 @@
 import Meta from '@/components/Meta'
 import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
+import Layout from '@/components/Layout'
 
 export default function Investments() {
   return (
     <>
     <Meta />
-    <NavBar />
-    <Footer />
+    <Layout>
+      
+    </Layout>
     </>
   )
 }
--- pages/marketplace.tsx
+++ pages/marketplace.tsx
@@ -2,35 +2,36 @@ import NavBar from '@/components/NavBar'
 import Meta from '@/components/Meta'
 import Footer from '@/components/Footer'
 import ItemMarket from '@/components/ItemMarket'
+import Layout from '@/components/Layout'
 export default function marketplace () {
     return (
     <>
         <Meta />
-        <NavBar />
-        <div className="flex bg-[url('/fondo1.png')] bg-cover h-96 w-full p-16 justify-center flex-wrap wra">
-            <div className='flex mt-36 w-4/5'>
-                <input id='card' className='h-14 w-4/5 rounded-l-xl p-5'>
-                </input>
-                <div className='flex w-1/5 h-14 bg-red-400 justify-center items-center rounded-r-lg'>
-                    <span className='text-2xl text-white font-medium'>Buscar</span>
+        <Layout>
+            <div className="flex bg-[url('/fondo1.png')] bg-cover h-96 w-full p-16 justify-center flex-wrap wra">
+                <div className='flex mt-36 w-4/5'>
+                    <input id='card' className='h-14 w-4/5 rounded-l-xl p-5'>
+                    </input>
+                    <div className='flex w-1/5 h-14 bg-red-400 justify-center items-center rounded-r-lg'>
+                        <span className='text-2xl text-white font-medium'>Buscar</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div>
-        </div>
-        <div className='flex flex-col bg-gray-200'>
-            <div className='flex p-10 pt-20 pl-20'>
-                <span className='text-3xl font-semibold'>Resultados de Búsqueda</span>
+            <div>
             </div>
-            <div className='flex p-10 py-16 justify-evenly flex-wrap'>
-                <ItemMarket />
-                <ItemMarket />
-                <ItemMarket />
-                <ItemMarket />
+            <div className='flex flex-col bg-gray-200'>
+                <div className='flex p-10 pt-20 pl-20'>
+                    <span className='text-3xl font-semibold'>Resultados de Búsqueda</span>
+                </div>
+                <div className='flex p-10 py-16 justify-evenly flex-wrap'>
+                    <ItemMarket />
+                    <ItemMarket />
+                    <ItemMarket />
+                    <ItemMarket />
+                </div>
             </div>
-        </div>
+        </Layout>
 
-        <Footer />
     </>
     )
 }
