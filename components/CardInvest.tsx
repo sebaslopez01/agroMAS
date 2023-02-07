e4fef84524cc4dc8f5ed9ea0d82c4451fc150ca6
abimont <abimontes00@gmail.com>
2023-02-06 20:48:29 -0500

    Update responsive design

--- components/CardInvest.tsx
+++ components/CardInvest.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 export default function CardInvest() {
   return (
     <>
-      <div id="cardInvest" className="h-fit w-[33%] rounded-xl">
+      <div id="cardInvest" className="h-fit w-[100%] md:w-[80%] lg:w-[47%] xl:w-[32%] rounded-xl m-2 p-1">
         <div className="flex flex-row">
           <Avatar />
           <div className="flex flex-col justify-center">
-            <span className="text-5xl font-medium">María Pérez</span>
+            <span className="text-3xl font-medium">María Pérez</span>
             <div className="flex flex-row">
-              <Image src='/location.png' alt="location" height={5} width={30}></Image>
-              <span className="text-2xl">Cali, Valle</span>
+              <Image src='/location.png' alt="location" height={1} width={30} className="scale-75"></Image>
+              <span className="text-xl">Cali, Valle</span>
             </div>
           </div>
         </div>
