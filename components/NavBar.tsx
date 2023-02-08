43052dc1e634d5769b5eec5d425476eea946028a
MiguelGit20 <miguel.torres.20@hotmail.com>
2023-02-08 07:35:40 -0500

    Update: responsive design

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,14 +4,14 @@ import { useState } from "react"
 import Avatar from "./Avatar"
 
 export default function NavBar() {
-    
+
     const [menu, setMenu] = useState(true)
 
     const handleMenu = () => {
         setMenu(!menu)
     }
     return (
-        <div id="bg-Navbar" className="w-[100%] flex justify-between items-center z-50">
+        <div id="bg-Navbar" className="w-[100%] flex justify-between items-center p-5 z-50">
 
             {/* Nostros - Invierte */}
             <div className="w-[33%] space-x-16 pl-10 hidden lg:flex">
@@ -20,23 +20,24 @@ export default function NavBar() {
             </div>
 
             {/* Agro logo */}
-            <Link href="/"><Image className="scale-75" src="/agro-logo.png" alt="Agro-logo" width={200} height={200} /></Link>
-            
-            {/* Menu Icon */}
-            <div className="p-5 lg:hidden text-white" onClick={handleMenu}>
-                {!menu ? 'X' : '|||'}
+            <Link href="/"><Image className="md:w-[150px]" src="/agro-logo.png" alt="Agro-logo" width={100} height={100} /></Link>
+
+            {/* Menu and Cart icon */}
+            <div className="flex justify-end items-center space-x-5 w-[33%] lg:hidden text-white md:justify-evenly" onClick={handleMenu}>
+                <Link href='marketplace'><Image className="md:w-[50px]"  src="/cart-icon.png" alt="Menu-icon" width={35} height={35} /></Link>
+                {!menu ? <Image className="md:w-[35px]" src="/close-icon.png" alt="Agro-logo" width={25} height={25} />: <Image className="md:w-[35px]" src="/menu-icon.png" alt="Agro-logo" width={25} height={25} /> }
             </div>
 
-            {/* Menu */}
-            <div className={!menu ? "fixed top-0 left-0 bg-red-200 w-[80%] h-screen p-5 ease-in-out duration-500 lg:hidden" : "fixed top-[-100%]" }>
-                <div className="flex justify-between mb-5">
-                    <h1 className="text-4xl font-semibold">Menú</h1>
+            {/* Menu flexible*/}
+            <div id="card" className={!menu ? "fixed top-0 left-0 w-[70%] h-screen p-5 ease-in-out duration-500 lg:hidden" : "fixed top-[-100%]"}>
+                <div className="w-[100%] mb-5 flex justify-start md:p-5">
+                    <Link href="/"><Image className="md:scale-150" src="/agro-logo.png" alt="Agro-logo" width={100} height={100} /></Link>
                 </div>
                 <ul>
-                    <li className="py-2"><Link href="profile" className="text-2xl mt-5">Iniciar sesión</Link></li>
-                    <li className="py-2"><Link href="nosotros" className="text-2xl mt-5">Inicio</Link></li>
-                    <li className="py-2"><Link href="nosotros" className="text-2xl mt-5">Nosotros</Link></li>
-                    <li className="py-2"><Link href="investments" className="text-2xl mt-5">Invierte</Link></li>
+                    <li className="py-2"><Link href="profile" className="text-2xl mt-5 text-white md:text-3xl">Iniciar sesión</Link></li>
+                    <li className="py-2"><Link href="nosotros" className="text-2xl mt-5 text-white md:text-3xl">Inicio</Link></li>
+                    <li className="py-2"><Link href="nosotros" className="text-2xl mt-5 text-white md:text-3xl">Nosotros</Link></li>
+                    <li className="py-2"><Link href="investments" className="text-2xl mt-5 text-white md:text-3xl">Invierte</Link></li>
                 </ul>
             </div>
 
--- pages/marketplace.tsx
+++ pages/marketplace.tsx
@@ -3,35 +3,46 @@ import Meta from '@/components/Meta'
 import Footer from '@/components/Footer'
 import ItemMarket from '@/components/ItemMarket'
 import Layout from '@/components/Layout'
-export default function marketplace () {
+import Image from 'next/image'
+
+
+export default function marketplace() {
     return (
-    <>
-        <Meta />
-        <Layout>
-            <div className="flex bg-[url('/fondo1.png')] bg-cover h-96 w-full p-16 justify-center flex-wrap wra">
-                <div className='flex mt-36 w-4/5'>
-                    <input id='card' className='h-14 w-4/5 rounded-l-xl p-5'>
-                    </input>
-                    <div className='flex w-1/5 h-14 bg-red-400 justify-center items-center rounded-r-lg'>
-                        <span className='text-2xl text-white font-medium'>Buscar</span>
+        <>
+            <Meta />
+            <Layout>
+                <div className="flex flex-col justify-center items-center w-full h-52 space-y-3 bg-[url('/fondo1.png')] bg-cover p-5">
+                    {/* Title Marketplace */}
+                    <div className='flex justify-start w-full mt-24'>
+                        <h1 id='shadow' className='text-white text-3xl'>Tienda</h1>
+                    </div>
+
+                    {/* Search */}
+                    <div className='flex w-full h-10 mb-3'>
+
+                        <input id='card' className='w-4/5 rounded-l-xl p-5 focus:outline-none'>
+                        </input>
+                        <button className='flex w-1/5 bg-red-400 justify-center items-center rounded-r-lg'>
+                            {/* <span className='text-2xl text-white font-medium'>Buscar</span> */}
+                            <Image className='scale-75' src="/search-icon.png" alt="" width={50} height={50}></Image>
+                        </button>
                     </div>
                 </div>
-            </div>
-            <div>
-            </div>
-            <div className='flex flex-col bg-gray-200'>
-                <div className='flex p-10 pt-20 pl-20'>
-                    <span className='text-3xl font-semibold'>Resultados de Búsqueda</span>
+                <div>
                 </div>
-                <div className='flex p-10 py-16 justify-evenly flex-wrap'>
-                    <ItemMarket />
-                    <ItemMarket />
-                    <ItemMarket />
-                    <ItemMarket />
+                <div className='flex flex-col bg-gray-200'>
+                    <div className='flex w-[90%] justify-start m-auto mt-7 mb-4'>
+                        <span className='text-2xl font-semibold'>Resultados de búsqueda...</span>
+                    </div>
+                    <div className='flex flex-wrap justify-center w-full'>
+                        <ItemMarket />
+                        <ItemMarket />
+                        <ItemMarket />
+                        <ItemMarket />
+                    </div>
                 </div>
-            </div>
-        </Layout>
+            </Layout>
 
-    </>
+        </>
     )
 }
