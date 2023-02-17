aa84856cd99c01c8dd068254abd75ad62db2fe0e
abimont <abimontes00@gmail.com>
2023-02-17 18:20:40 -0500

    Profile settings

--- pages/profile/seller/settings.tsx
+++ pages/profile/seller/settings.tsx
@@ -3,7 +3,7 @@ import Meta from "@/components/Meta";
 import NavBarMarket from "@/components/navigation/NavBarMarket";
 import Footer from "@/components/navigation/Footer";
 import Image from "next/image";
-import pic from "@/public/person.jpg"
+import pic from "@/public/person.jpg";
 
 export default function Settings() {
   return (
@@ -16,63 +16,106 @@ export default function Settings() {
 
         {/* first line */}
         <div className="flex w-[100%]">
-          <Image src={pic} alt="profile pic" className="w-[20%] rounded-3xl p-1"/>
-          <div className="flex flex-col w-[40%] p-1">
-            <span>Nombres</span>
-            <input type="text" className="border border-gray-300 rounded-full" />
-          </div>
+          <Image
+            src={pic}
+            alt="profile pic"
+            className="w-[20%] rounded-3xl p-1"
+          />
 
-          <div className="flex flex-col w-[40%] p-1">
-            <span>Apellidos</span>
-            <input type="text"  className="border border-gray-300 rounded-full" />
+          <div className="flex flex-col w-[80%]">
+            <div className="flex w-[100%]">
+              <div className="flex flex-col w-[50%] p-1">
+                <span>Nombres</span>
+                <input
+                  type="text"
+                  className="border border-gray-300 rounded-full"
+                />
+              </div>
+
+              <div className="flex flex-col w-[50%] p-1">
+                <span>Apellidos</span>
+                <input
+                  type="text"
+                  className="border border-gray-300 rounded-full"
+                />
+              </div>
+            </div>
+
+            <div className="flex w-[100%]">
+              <div className="flex flex-col w-[50%] p-1">
+                <span>Tipo y número de documento</span>
+                <div className="flex">
+                  <input
+                    type="text"
+                    className="border w-[30%] mr-1 border-gray-300 rounded-full"
+                  />
+                  <input
+                    type="text"
+                    className="border w-[70%] ml-1 border-gray-300 rounded-full"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col w-[50%] p-1">
+                <span>Número de teléfono</span>
+                <input
+                  type="text"
+                  className="border border-gray-300 rounded-full"
+                />
+              </div>
+            </div>
           </div>
         </div>
 
         {/* second line */}
-        <div className="flex w-[100%]">
-          <div className="flex flex-col w-[50%] p-1">
-            <span>Tipo y número de documento</span>
-            <div className="flex">
-              <input type="text" className="border w-[30%] mr-1 border-gray-300 rounded-full" />
-              <input type="text" className="border w-[70%] ml-1 border-gray-300 rounded-full" />
-            </div>
-          </div>
-
-          <div className="flex flex-col w-[50%] p-1">
-            <span>Número de teléfono</span>
-            <input type="text" className="border border-gray-300 rounded-full" />
-          </div>
-        </div>
+        <div className="flex w-[100%]"></div>
 
         {/* third line */}
         <div className="flex w-[100%] justify-between">
           <div className="flex flex-col w-[33%] p-1">
             <span>Correo electrónico</span>
-            <input type="text" className="border border-gray-300 rounded-full" />
+            <input
+              type="text"
+              className="border border-gray-300 rounded-full"
+            />
           </div>
           <div className="flex flex-col w-[33%] p-1">
             <span>Contraseña</span>
-            <input type="text" className="border border-gray-300 rounded-full" />
+            <input
+              type="text"
+              className="border border-gray-300 rounded-full"
+            />
           </div>
           <div className="flex flex-col w-[33%] p-1">
             <span>Rol</span>
-            <input type="text" className="border border-gray-300 rounded-full" />
+            <input
+              type="text"
+              className="border border-gray-300 rounded-full"
+            />
           </div>
         </div>
-        
+
         {/* fourth line */}
         <div className="flex w-[100%]">
           <div className="flex flex-col w-[50%] p-1">
             <span>Dirección</span>
-            <input type="text" className="border border-gray-300 rounded-full" />
+            <input
+              type="text"
+              className="border border-gray-300 rounded-full"
+            />
           </div>
           <div className="flex flex-col w-[25%] p-1">
             <span>Ciudad</span>
-            <input type="text" className="border border-gray-300 rounded-full" />
+            <input
+              type="text"
+              className="border border-gray-300 rounded-full"
+            />
           </div>
           <div className="flex flex-col w-[25%] p-1">
             <span>Departamento</span>
-            <input type="text" className="border border-gray-300 rounded-full" />
+            <input
+              type="text"
+              className="border border-gray-300 rounded-full"
+            />
           </div>
         </div>
       </div>
