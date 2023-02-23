6c961f2f221bc892541679b96499ee568f3d07ab
MiguelGit20 <miguel.torres.20@hotmail.com>
2023-02-23 13:25:42 -0500

    update: section us

--- pages/nosotros.tsx
+++ pages/nosotros.tsx
@@ -1,13 +1,21 @@
-import Layout from "@/components/Layout"
+import Footer from "@/components/navigation/Footer";
+import NavbarGeneral from "@/components/navigation/NavbarGeneral";
+import Navbar from "@/components/navigation/NavBar";
 
-export default function nosotros(){
-    return (
-        <Layout>
-            <div>
-                <div className="w-full h-[96px]"></div>
-                <button className="bg-gray-500 p-3 m-3 rounded-lg" onClick={() => window.history.back()}> &lt;---- Pa tra</button>
-            </div>
-        </Layout>
-        
-    )
+export default function nosotros() {
+  return (
+    <>
+      {/* <NavbarGeneral namePage="Nosotros"/> */}
+      <Navbar />
+      <div className="w-full h-[200px] bg-[url('/backgrounds/4.jpg')] bg-cover bg-center mx-auto flex flex-col justify-end px-10 py-5">
+        <h2 className="text-white text-5xl font-semibold">Nosotros</h2>
+      </div>
+      <div className="w-[80%] mx-auto h-auto mt-10 text-gray-700 text-lg">
+        <p className="text-center">
+          AgroMAS nace como una iniciativa para conectar el campo con las personas
+        </p>
+      </div>
+      <Footer />
+    </>
+  );
 }
